ece4cb3a2c03284c0f53a80e69b100fa257c4648
kking426g <kking426g@gmail.com>
2018-01-17 22:55:59 +0800

    first push

---
+++ shoppingpage/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+
+import { AppComponent } from './app.component';
+import { ItemsComponent } from './components/items/items.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'shoppingpage'),
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ shoppingpage/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAdCuRP2D_bnJ-hsu7CSMzpO44wAXS8LdI",
+    authDomain: "shoppingpage-773b6.firebaseapp.com",
+    databaseURL: "https://shoppingpage-773b6.firebaseio.com",
+    projectId: "shoppingpage-773b6",
+    storageBucket: "",
+    messagingSenderId: "219012140231"
+  }
+};
