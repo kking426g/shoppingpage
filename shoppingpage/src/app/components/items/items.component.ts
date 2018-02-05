b3176e03688fe4f8423ccf3edcbd375e516fa670
Joanne Wu <joannewu@astrocorp.com.tw>
2018-02-05 17:21:11 +0800

    download image from firebase storage

--- shoppingpage/src/app/components/items/items.component.ts
+++ shoppingpage/src/app/components/items/items.component.ts
@@ -7,30 +7,22 @@ import * as firebase from 'firebase/app'; // for typings
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
-  styleUrls: ['./items.component.css'],
-  template: '<img [src]="image">'
+  styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
   image: string;
-  constructor(private itemService: ItemService, firebaseApp: FirebaseApp) {
-
-// Get a reference to the storage service, which is used to create references in your storage bucket
-var storage = firebaseApp.storage();
-// Create a storage reference from our storage service
-var storageRef = storage.ref();
-// Create a child reference
-var imagesRef = storageRef.child('images');
-// Child references can also take paths delimited by '/'
-var spaceRef = storageRef.child('images/space.jpg');
-
-    //const storageRef = firebaseApp.storage().ref().child('images/1.jpg');
-    spaceRef.getDownloadURL().then(url => this.image = url);
+  constructor(private itemService: ItemService, private firebaseApp: FirebaseApp) {
   }
 
   ngOnInit() { //lifecycle function
     this.itemService.getItems().subscribe(items =>{
       console.log(items);
     })
+
+    this.firebaseApp.storage().ref('images/1.jpg').getDownloadURL().then(function(url) {
+      console.log("la url ", url)
+    })
+
   }
 
 }
