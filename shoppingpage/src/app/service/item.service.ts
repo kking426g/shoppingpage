fe70fc9cc0df2bd703ccb0674558c4e56efe4af5
kking426g <kking426g@gmail.com>
2018-02-05 23:42:08 +0800

    update items component

--- shoppingpage/src/app/service/item.service.ts
+++ shoppingpage/src/app/service/item.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
+import { FirebaseApp } from 'angularfire2';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from'angularfire2/firestore';
 import { Item } from '../models/Item';
 import { Observable } from 'rxjs/Observable';//需要這個使資料observable
+import 'firebase/storage';
+import * as firebase from 'firebase/app'; // for typings
 @Injectable()
 export class ItemService {
   itemCollection: AngularFirestoreCollection<Item>;//data string
   items: Observable<Item[]>;//因為要有這個 所以要加入observable  initalizse as abservable with typeof item
-
+  itemimg: AngularFirestoreCollection<Item>;
   constructor(public afs: AngularFirestore) { 
-    this.items = this.afs.collection('items').valueChanges();//指的是上面observable的items  set into a collection
+    this.items = this.afs.collection('items').valueChanges();//指的是上面observable的items  set into a collection 
   }
   
   getItems(){
     return this.items;
   }
-
 }
-
-
 
